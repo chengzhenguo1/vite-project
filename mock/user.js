a8512c5e8db5598592775256f38e49687d7dd467
czg_620 <943944010@qq.com>
2021-02-03 17:58:36 +0800

    切换分支

--- mock/user.js
+++ mock/user.js
@@ -11,3 +11,22 @@ export default [
     },
   },
 ];
+
+  
+/* export default [
+  {
+    url: '/api/getRoleById',
+    method: 'get',
+    response: ({ query }) => {
+      console.log('id>>>>>>>>', query.id);
+      return {
+        code: 0,
+        message: 'ok',
+        data: {
+          roleName: 'admin',
+          roleValue: 'admin',
+        },
+      };
+    },
+  },
+]; */
